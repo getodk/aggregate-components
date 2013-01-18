b2162270ab8f6c69a0348456ca6c1f7188fc65df
mitchellsundt <mitchellsundt@gmail.com>
2013-01-18 11:10:07 -0800

    Change test to allow newer spring framework; minor tweak to use newest apache httpclient

---
+++ spring-security-patch/core/src/test/java/org/springframework/security/core/SpringSecurityCoreVersionTests.java
@@ -0,0 +1,34 @@
+package org.springframework.security.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+/**
+ * Checks that the embedded version information is up to date.
+ *
+ * @author Luke Taylor
+ */
+public class SpringSecurityCoreVersionTests {
+
+    @Test
+    public void springVersionIsUpToDate() throws Exception {
+        // Property is set by the build script
+        String springVersion = System.getProperty("springVersion");
+
+		assertTrue( springVersion.compareTo(SpringSecurityCoreVersion.MIN_SPRING_VERSION) >= 0 );
+    }
+
+    @Test
+    public void serialVersionMajorAndMinorVersionMatchBuildVersion() throws Exception {
+        String version = System.getProperty("springSecurityVersion");
+
+        // Strip patch version
+        String serialVersion = String.valueOf(SpringSecurityCoreVersion.SERIAL_VERSION_UID).substring(0,2);
+
+        assertEquals(version.charAt(0), serialVersion.charAt(0));
+        assertEquals(version.charAt(2), serialVersion.charAt(1));
+
+    }
+}
